f9408f242e2223c818dec5872bfc64f436890e99
ddolcimascolo <ddolcimascolo@linagora.com>
2017-07-21 09:16:36 +0200

    Always chunk midway tests.

--- tasks/tests.js
+++ tasks/tests.js
@@ -1,9 +1,8 @@
 'use strict';
 
 module.exports = function(grunt) {
-
   grunt.registerMultiTask('splitfiles', 'split the files and run separate targets', function() {
-    var options = this.options({
+    const options = this.options({
       chunk: 50,
       common: []
     });
@@ -14,24 +13,15 @@ module.exports = function(grunt) {
       return;
     }
 
-    var files = this.files.reduce(function(a, b) {
-      return a.concat(b.src);
-    }, []);
-    var totalFiles = files.length;
-    var chunkSize = grunt.option('chunk');
+    const files = this.files.reduce((a, b) => a.concat(b.src), []),
+          totalFiles = files.length,
+          chunkSize = options.chunk,
+          commonFiles = grunt.file.expand(options.common),
+          targets = [],
+          configBase = options.target.replace(/:/g, '.');
 
-    if (chunkSize === true) {
-      chunkSize = options.chunk;
-    } else if (typeof chunkSize === 'undefined') {
-      chunkSize = totalFiles;
-    }
-
-    var commonFiles = grunt.file.expand(options.common);
-    var targets = [];
-    var configBase = options.target.replace(/:/g, '.');
-
-    for (var chunkId = 1; files.length; chunkId++) {
-      var chunkFiles = commonFiles.concat(files.splice(0, chunkSize));
+    for (let chunkId = 1; files.length; chunkId++) {
+      const chunkFiles = commonFiles.concat(files.splice(0, chunkSize));
 
       grunt.config.set(configBase + chunkId + '.options.files', chunkFiles);
       targets.push(options.target + chunkId);
@@ -40,6 +30,7 @@ module.exports = function(grunt) {
     if (targets.length > 1) {
       grunt.log.ok('Splitting ' + totalFiles + ' tests into ' + targets.length + ' chunks');
     }
+
     grunt.task.run(targets);
   });
 };
