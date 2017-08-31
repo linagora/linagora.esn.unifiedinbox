b4e6afbd08d875e2a2b2035259776daf59efc902
fmoyon <fmoyon@linagora.com>
2017-08-31 11:32:31 +0200

    Fixes #314 new component folder settings options

---
+++ frontend/app/components/sidebar/folder-settings/folder-settings.component.js
@@ -0,0 +1,13 @@
+(function() {
+'use strict';
+
+  angular.module('linagora.esn.unifiedinbox')
+    .component('inboxFolderSettings', {
+      templateUrl: '/unifiedinbox/app/components/sidebar/folder-settings/folder-settings.html',
+      bindings: {
+        isFolder: '=',
+        mailbox: '='
+      },
+      controllerAs: 'ctrl'
+    });
+})();
