32e80ec94261a0206a34685d5311d61a9a5c2b0c
ddolcimascolo <ddolcimascolo@linagora.com>
2018-02-01 09:44:42 +0100

    #806. New mailto Angular application.

---
+++ frontend/app/mailto/mailto.run.js
@@ -0,0 +1,20 @@
+(function(angular) {
+  'use strict';
+
+  angular
+    .module('linagora.esn.unifiedinbox.mailto')
+    .run(function($location, $window, $timeout, sessionFactory, newComposerService, StateManager, inboxMailtoParser, INBOX_MAILTO_AUTOCLOSE_DELAY) {
+      sessionFactory.fetchUser(function() {
+        newComposerService.open(inboxMailtoParser($location.path().replace(/^\/.+\//, '')), {
+          postSendCallback: function() {
+            $timeout($window.close.bind($window), INBOX_MAILTO_AUTOCLOSE_DELAY);
+          }
+        }, {
+          closeable: false,
+          allowedStates: [],
+          initialState: StateManager.STATES.FULL_SCREEN
+        });
+      });
+    });
+
+})(angular);
---
+++ frontend/app/mailto/mailto.spec.js
@@ -0,0 +1,94 @@
+'use strict';
+
+/* global chai: false, sinon: false */
+
+var expect = chai.expect;
+
+describe('The mailto Angular application', function() {
+
+  beforeEach(function() {
+    module('linagora.esn.unifiedinbox.mailto');
+  });
+
+  it('should create a user session', function(done) {
+    module(function($provide) {
+      $provide.value('sessionFactory', {
+        fetchUser: function() {
+          done();
+        }
+      });
+    });
+
+    inject();
+  });
+
+  it('should open a composer, delegating URL parsing to inboxMailtoParser', function() {
+    var message = {
+      to: [{
+        name: 'name',
+        email: 'email'
+      }]
+    };
+
+    module(function($provide) {
+      $provide.decorator('$location', function($delegate) {
+        $delegate.path = function() {
+          return 'mailto:testing@linagora.com';
+        };
+
+        return $delegate;
+      });
+      $provide.value('inboxMailtoParser', function(url) {
+        expect(url).to.equal('mailto:testing@linagora.com');
+
+        return message;
+      });
+      $provide.value('newComposerService', { open: sinon.spy() });
+      $provide.value('sessionFactory', {
+        fetchUser: function(callback) {
+          callback();
+        }
+      });
+    });
+
+    inject(function(newComposerService, StateManager) {
+      expect(newComposerService.open).to.have.been.calledWith(message, {
+        postSendCallback: sinon.match.func
+      }, {
+        closeable: false,
+        allowedStates: [],
+        initialState: StateManager.STATES.FULL_SCREEN
+      });
+    });
+  });
+
+  it('should automatically close the window when the message is sent', function() {
+    var message = {};
+
+    module(function($provide) {
+      $provide.decorator('$window', function($delegate) {
+        $delegate.close = sinon.spy();
+
+        return $delegate;
+      });
+      $provide.value('inboxMailtoParser', function() { return message; });
+      $provide.value('newComposerService', {
+        open: function(message, compositionOptions) {
+          compositionOptions.postSendCallback();
+        }
+      });
+      $provide.value('sessionFactory', {
+        fetchUser: function(callback) {
+          callback();
+        }
+      });
+    });
+
+    inject(function($timeout, $window) {
+      $timeout.flush();
+
+      expect($window.close).to.have.been.calledWith();
+    });
+  });
+
+});
