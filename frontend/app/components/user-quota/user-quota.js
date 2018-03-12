9fd721827ba650d186ad8b5687e7f7568c4983c8
Fabien Vignon <fvignon@linagora.com>
2018-03-12 16:50:07 +0100

    #813 add inbox-sidebar-user-quota component
    - to left-hand sidebar
    - with inboxUserQuotaService

---
+++ frontend/app/components/user-quota/user-quota.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  angular.module('linagora.esn.unifiedinbox')
+
+    .component('inboxSidebarUserQuota', {
+      templateUrl: '/unifiedinbox/app/components/user-quota/user-quota.html',
+      controller: 'inboxSidebarUserQuotaController'
+    });
+
+})();
