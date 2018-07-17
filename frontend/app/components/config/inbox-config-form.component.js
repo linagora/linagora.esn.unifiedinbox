c7e20719eb2df4bb1a11edfd9ec63f66d079cd5e
Khac Chien LANG <kclang@linagora.com>
2018-07-17 20:12:03 +0700

    #916 call dedicated API in Inbox module to update forward configurations

--- frontend/app/components/config/inbox-config-form.component.js
+++ frontend/app/components/config/inbox-config-form.component.js
@@ -2,12 +2,14 @@
   'use strict';
 
   angular.module('linagora.esn.unifiedinbox')
-
-  .component('inboxConfigForm', {
-    templateUrl: '/unifiedinbox/app/components/config/inbox-config-form.html',
-    bindings: {
-      configurations: '='
-    }
-  });
-
+    .component('inboxConfigForm', {
+      templateUrl: '/unifiedinbox/app/components/config/inbox-config-form.html',
+      controller: 'InboxConfigFormController',
+      bindings: {
+        configurations: '<'
+      },
+      require: {
+        adminModulesDisplayerController: '^adminModulesDisplayer'
+      }
+    });
 })(angular);
