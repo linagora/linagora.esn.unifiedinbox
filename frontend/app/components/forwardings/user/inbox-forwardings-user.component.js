ba417a7602251af734ec22e9c49e470ed696d9da
vugingerbon <vuducnghia1996@gmail.com>
2018-08-24 14:20:27 +0700

    linagora.esn.admin#440 allows domain administrator to set email redirections for domain member in frontend

---
+++ frontend/app/components/forwardings/user/inbox-forwardings-user.component.js
@@ -0,0 +1,11 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.unifiedinbox')
+    .component('inboxForwardingsUser', {
+      templateUrl: '/unifiedinbox/app/components/forwardings/user/inbox-forwardings-user.html',
+      bindings: {
+        user: '<'
+      }
+    });
+})(angular);
--- frontend/app/services/forwardings/inbox-forwardings.service.spec.js
+++ frontend/app/services/forwardings/inbox-forwardings.service.spec.js
@@ -55,4 +55,46 @@ describe('The inboxForwardingsService service', function() {
       $rootScope.$digest();
     });
   });
+
+  describe('The updateUserForwardings function', function() {
+    it('should call inboxForwardingClient.addUserForwarding to add new forwardings', function(done) {
+      inboxForwardingClient.addUserForwarding = sinon.spy();
+      var updateData = {
+        forwardingsToAdd: ['email1@op.org', 'email2@op.org'],
+        forwardingsToRemove: []
+      };
+      var userId = 'ui123';
+      var domainId = '123456';
+
+      inboxForwardingsService.updateUserForwardings(updateData, userId, domainId)
+        .then(function() {
+          expect(inboxForwardingClient.addUserForwarding).to.have.been.calledWith('email1@op.org', userId, domainId);
+          expect(inboxForwardingClient.addUserForwarding).to.have.been.calledWith('email2@op.org', userId, domainId);
+          expect(inboxForwardingClient.addUserForwarding).to.have.been.calledTwice;
+
+          done();
+        });
+      $rootScope.$digest();
+    });
+
+    it('should call inboxForwardingClient.removeUserForwarding to remove forwardings', function(done) {
+      inboxForwardingClient.removeUserForwarding = sinon.spy();
+      var updateData = {
+        forwardingsToAdd: [],
+        forwardingsToRemove: ['email1@op.org', 'email2@op.org']
+      };
+      var userId = 'ui123';
+      var domainId = '123456';
+
+      inboxForwardingsService.updateUserForwardings(updateData, userId, domainId)
+        .then(function() {
+          expect(inboxForwardingClient.removeUserForwarding).to.have.been.calledWith('email1@op.org', userId, domainId);
+          expect(inboxForwardingClient.removeUserForwarding).to.have.been.calledWith('email2@op.org', userId, domainId);
+          expect(inboxForwardingClient.removeUserForwarding).to.have.been.calledTwice;
+
+          done();
+        });
+      $rootScope.$digest();
+    });
+  });
 });
