cd47742f021e021d32c8477798b477f0afd9388c
Fabien MOYON <fmoyon@linagora.com>
2019-04-17 17:19:32 +0200

    #2707 Disabling unifiedinbox module from configuration

--- backend/webserver/api/identities/index.js
+++ backend/webserver/api/identities/index.js
@@ -2,8 +2,15 @@
 
 const express = require('express');
 
-module.exports = dependencies => {
+module.exports = (dependencies, moduleName) => {
   const router = express.Router();
+  const authorizationMW = dependencies('authorizationMW');
+  const moduleMW = dependencies('moduleMW');
+
+  router.all('/default*',
+    authorizationMW.requiresAPILogin,
+    moduleMW.requiresModuleIsEnabledInCurrentDomain(moduleName)
+  );
 
   router.get('/default', require('./controller')(dependencies).getDefaultIdentity);
 
