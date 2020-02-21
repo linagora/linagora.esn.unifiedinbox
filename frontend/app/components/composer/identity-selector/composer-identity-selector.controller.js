8760355b2300b46504497b62ee6ba867df9e89e7
Tuan Tuan LE <tlcong@linagora.com>
2020-02-21 16:38:02 +0700

    #1169 filter the list of usable identities on From field

--- frontend/app/components/composer/identity-selector/composer-identity-selector.controller.js
+++ frontend/app/components/composer/identity-selector/composer-identity-selector.controller.js
@@ -2,36 +2,33 @@
   'use strict';
 
   angular.module('linagora.esn.unifiedinbox')
+    .controller('inboxComposerIdentitySelectorController', inboxComposerIdentitySelectorController);
 
-    .controller('inboxComposerIdentitySelectorController', function(_, inboxIdentitiesService) {
-      var self = this;
+  function inboxComposerIdentitySelectorController(_, inboxIdentitiesService) {
+    var self = this;
 
-      self.$onInit = $onInit;
-      self.onIdentityChange = onIdentityChange;
-      self.getIdentityLabel = getIdentityLabel;
+    self.$onInit = $onInit;
+    self.onIdentityChange = onIdentityChange;
+    self.getIdentityLabel = getIdentityLabel;
 
-      /////
+    function $onInit() {
+      inboxIdentitiesService.getAllIdentities()
+        .then(function(identities) {
+          self.identities = identities.filter(function(identity) {
+            return identity.usable;
+          });
 
-      function $onInit() {
-        inboxIdentitiesService.getAllIdentities()
-          .then(function(identities) {
-            self.identities = identities;
+          self.identity = self.identity || _.find(self.identities, { default: true }) || self.identities[0];
+        })
+        .then(onIdentityChange);
+    }
 
-            // This will be improved in the future if we support a "preferred" identity (which might not be the default one)
-            // For now we always pre-select the default identity
-            self.identity = _.find(identities, self.identity ? { uuid: self.identity.uuid } : { default: true });
-          })
-          .then(onIdentityChange);
-      }
+    function onIdentityChange() {
+      self.onIdentityUpdate({ $identity: self.identity });
+    }
 
-      function onIdentityChange() {
-        self.onIdentityUpdate({ $identity: self.identity });
-      }
-
-      function getIdentityLabel(identity) {
-        return identity.name + ' <' + identity.email + '>';
-      }
-
-    });
-
+    function getIdentityLabel(identity) {
+      return identity.name + ' <' + identity.email + '>';
+    }
+  }
 })();
